66ee58fe842c4d71ce610f508b318bedd44c6ae2
Vatsal Jalan <jalanvatsal@gmail.com>
2024-05-15 23:23:33 -0700

    Added embedded video on home page

---
+++ client/src/components/aboutus.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const AboutUs = (props) => {
+
+    return (
+        <div>
+            <p>At OffBeat Sessions, we're passionate about building a thriving street dance community at UCLA. Our mission is to: </p>
+            
+            <div className="mission">
+                <ul>
+                    <li><strong>Foster Community:</strong> We believe in the power of dance to bring people together. Whether you're a seasoned vet or just starting out, you'll find a welcoming and inclusive space here at OffBeat Sessions. Join us as we connect, inspire, and support one another on our dance journeys.</li>
+                    <li><strong>Promote Freestyle Dance Culture:</strong> Freestyle is more than just movement – it's a form of self-expression and creativity. At OffBeat Sessions, we're dedicated to promoting and cultivating the development of freestyle dance culture at UCLA. Through sessions, workshops, and events we empower dancers to explore their artistry and push the boundaries of their craft.</li>
+                    <li><strong>Build Connections:</strong> We stress the importance of community and collaboration in the street dance world. That's why we're committed to forging connections with freestyle dance communities beyond UCLA. By fostering collaboration and growth in the broader dance community, we aim to create a vibrant and interconnected network of dancers.</li>
+                    <li><strong>All Styles Welcome:</strong> Whether you're into hip hop, popping, locking, breaking, animation, house, or any other style, you'll find a home here at OffBeat Sessions. Our community celebrates diversity and welcomes dancers of all backgrounds and skill levels.</li>
+                </ul>
+            </div>
+        </div>
+    );
+
+};
+
+export default AboutUs
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./home.css"
 
 const Home = (props) => {
     return (
         <div className={"mainContainer"}>
-            <p>
-                Welcome to OffBeat Sessions! UCLA's Only Street Dance Club
-            </p>
+
+            {/* Background video */}
+            <video autoPlay muted loop id="bgVideo">
+                <source src="/promo.webm" type="video/webm" />
+                {/* Add additional source elements for different video formats if needed */}
+                Your browser does not support the video tag.
+            </video>
+
+        <div className="textContainer">
+            <div className="welcomeMessage">
+                <p>Welcome to OffBeat Sessions! UCLA's Only Street Dance Club</p>
+            </div>
 
             <p>
                 New Here? <Link to="/signup">Signup here</Link>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {/* Adding space */}
                 Already a member? <Link to="/login">Click here</Link>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {/* Adding space */}
+                Learn More: <Link to="/aboutus">About Us</Link>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {/* Adding space */}
                 Follow us on Instagram: <a href="https://www.instagram.com/uclaoffbeat/">@uclaoffbeat</a>
             </p>
-            
+
+
+            <p>Join us as we dance, connect, and grow together!</p>
+
+        </div>
+
         </div>
     );
 };
