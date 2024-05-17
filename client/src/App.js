6c1382dedcf9bd5401ef4b8c4164fbed6dbdcd59
Masahiro Yamaguchi <yagcksge25@gmail.com>
2024-05-16 17:05:49 -0700

    admin/ users/ user page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login.js";
 import SignUp from "./components/singup.js";
+
+import AdminPage from "./components/admin-page.js"
+import UserPage from "./components/users-page.js"
+
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/userspage" element={<UserPage />} />
+        <Route path="/adminpage" element={<AdminPage />} />
+        <Route path="/userpage" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
