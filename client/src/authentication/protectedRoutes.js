462efcfcd7fea1bb3a37d4a21048ac85ed7de044
parnian <parniangk2005@ucla.edu>
2024-05-19 23:52:31 -0700

    remove client-side console.logs

--- client/src/authentication/protectedRoutes.js
+++ client/src/authentication/protectedRoutes.js
@@ -3,10 +3,8 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = sessionStorage.getItem("token");
-  console.log("do we get into this function");
 
   if (!token) {
-    console.log("is this being called?");
     return <Navigate to="/login" replace />;
   }
 
