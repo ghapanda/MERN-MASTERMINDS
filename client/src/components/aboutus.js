7ad50cd2170d7b5905eece255bf7b7bbcce93e04
Vatsal Jalan <jalanvatsal@gmail.com>
2024-05-22 00:28:21 -0700

    Frontend for users

--- client/src/components/aboutus.js
+++ client/src/components/aboutus.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./aboutus.css";
+
 
 const AboutUs = (props) => {
   sessionStorage.clear();
 
   return (
-    <div>
-      <p>
-        At OffBeat Sessions, we're passionate about building a thriving street
-        dance community at UCLA. Our mission is to:{" "}
-      </p>
+    <div className="mainContainer">
+
+      {/* Background video */}
+      <video autoPlay muted loop id="bgVideo">
+        <source src="/colorful.mp4" type="video/mp4" />
+        {/* Add additional source elements for different video formats if needed */}
+        Your browser does not support the video tag.
+      </video>
 
       <div className="mission">
         <ul>
@@ -21,15 +26,15 @@ const AboutUs = (props) => {
             another on our dance journeys.
           </li>
           <li>
-            <strong>Promote Freestyle Dance Culture:</strong> Freestyle is more
-            than just movement – it's a form of self-expression and creativity.
+            <strong>We Promote Freestyle Dance Culture:</strong> Freestyle is more
+            than just movement: it's a form of self-expression and creativity.
             At OffBeat Sessions, we're dedicated to promoting and cultivating
             the development of freestyle dance culture at UCLA. Through
             sessions, workshops, and events we empower dancers to explore their
             artistry and push the boundaries of their craft.
           </li>
           <li>
-            <strong>Build Connections:</strong> We stress the importance of
+            <strong>We Build Connections:</strong> We stress the importance of
             community and collaboration in the street dance world. That's why
             we're committed to forging connections with freestyle dance
             communities beyond UCLA. By fostering collaboration and growth in
@@ -37,7 +42,7 @@ const AboutUs = (props) => {
             interconnected network of dancers.
           </li>
           <li>
-            <strong>All Styles Welcome:</strong> Whether you're into hip hop,
+            <strong>Welcome All Styles:</strong> Whether you're into hip hop,
             popping, locking, breaking, animation, house, or any other style,
             you'll find a home here at OffBeat Sessions. Our community
             celebrates diversity and welcomes dancers of all backgrounds and
@@ -45,7 +50,8 @@ const AboutUs = (props) => {
           </li>
         </ul>
       </div>
-    </div>
+
+      </div>
   );
 };
 
