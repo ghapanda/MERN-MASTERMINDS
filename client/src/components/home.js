6b09801b36464a136c1c8f2bcce22f0e42714e9a
Srinjana Sriram <srinjana.sriram@gmail.com>
2024-05-29 18:26:33 -0700

    front end of home page fixed

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,29 +7,27 @@ const Home = (props) => {
 
   return (
     <div className={"mainContainer"}>
-      {/* Background video */}
-      <video autoPlay muted loop id="bgVideo">
+      
+      <video autoPlay muted loop id="bgVideo" >
         <source src="/promo.webm" type="video/webm" />
-        {/* Add additional source elements for different video formats if needed */}
         Your browser does not support the video tag.
       </video>
 
-      <div className="textContainer">
-        
+      <div className="blurbContainer">
+        <p>Join us as we dance, connect, and grow together!</p>
+      </div>
+
+      <div className="linkContainer">
         <p>
-          New Here? <Link to="/signup">Signup here</Link>
+          New Here? <Link to="/signup">Signup Here</Link>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {/* Adding space */}
-          Already a member? <Link to="/login">Click here</Link>
+          Already a member? <Link to="/login">Login Here</Link>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {/* Adding space */}
           Learn More: <Link to="/aboutus">About Us</Link>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {/* Adding space */}
           Follow us on Instagram:{" "}
           <a href="https://www.instagram.com/uclaoffbeat/">@uclaoffbeat</a>
         </p>
-      </div>
-
-      <div className="ending">
-      <p>Join us as we dance, connect, and grow together!</p>
       </div>
 
     </div>
