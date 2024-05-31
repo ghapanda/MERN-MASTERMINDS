f50a53a1133586569fd4596eb3eeae410bb00ebe
Vatsal Jalan <jalanvatsal@gmail.com>
2024-05-30 19:08:42 -0700

    Switch branch

--- client/src/components/UserProfile/adminH.js
+++ client/src/components/UserProfile/adminH.js
@@ -5,7 +5,7 @@ import "./profile.css"
 const AdminH =()=>{
     return (
       <div className="header">
-        <h1 className="headerT">ucla offbeat</h1>
+        <h1 className="headerT">Offbeat Sessions</h1>
             <button className="button"><Link to="/dashboard" className="linkh"> Dashboard</Link></button>
             <button className="button"><Link to="/memberSchedulePage" className="linkh">Schedule</Link></button>
            {/* <button className="button"><Link to="/userspage" className="linkh">users</Link></button>*/}
