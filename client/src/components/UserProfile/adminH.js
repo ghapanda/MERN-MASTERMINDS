679948b4e0781925ee7518c628ed4163e4cc6a11
catsniffer47 <kimc5546@gmail.com>
2024-06-04 14:47:18 -0700

    Profile Dashboard/Logout Removal
    Removed the Dashboard and Logout buttons.

--- client/src/components/UserProfile/adminH.js
+++ client/src/components/UserProfile/adminH.js
@@ -8,8 +8,6 @@ const AdminH =()=>{
       <div className="header">
         <h1 className="headerT">ucla offbeat</h1>
             <Navbar />
-            <button className="button"><Link to="/dashboard" className="linkh"> Dashboard</Link></button>
-            <button className="button"><Link to="/memberSchedulePage" className="linkh">Schedule</Link></button>
            {/* <button className="button"><Link to="/userspage" className="linkh">users</Link></button>*/}
         </div>
     )
--- client/src/components/UserProfile/notAdminH.js
+++ client/src/components/UserProfile/notAdminH.js
@@ -9,18 +9,6 @@ const NotAdminH = () => {
         <h1 className="headerT">Offbeat Sessions</h1>
         <div>
           <Navbar />
-          <button className="button">
-            <Link to="/dashboard" className="linkh">
-              {" "}
-              Dashboard
-            </Link>
-          </button>
-          <button className="button">
-            <Link to="/" className="linkh">
-              {" "}
-              Logout
-            </Link>
-          </button>
         </div>
         {/*  <button className="button"><Link to="/userspage" className="linkh">users</Link></button>*/}
       </div>
